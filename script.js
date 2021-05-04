c108d43d8f84d97eaf84efffc0d796e127e73d7d
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-05-04 17:13:17 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const budgetDisplay = document.getElementById('budget-display');
+const budgetRemaining = document.getElementById('budget-remaining');
+const budgetInput = document.getElementById('budget-input');
+const budgetForm = document.getElementById('available-budget-form');
+
+const groceryForm = document.getElementById('grocery-entry-form');
+const groceryList = document.getElementById('grocery-list');
+const itemNameInput = document.getElementById('item');
+const itemPriceInput = document.getElementById('price');
+
+let groceryListData = [];
+let totalBudget;
+
+const updateBudget = (price) => {
+	const currentTotal = groceryListData.reduce(
+		(acc, cur) => cur.price + acc,
+		0
+	);
+
+	let currentBudget = totalBudget - currentTotal;
+
+	budgetRemaining.innerHTML = currentBudget.toFixed(2);
+
+	budgetDisplay.style.backgroundColor = currentBudget >= 0 ? 'green' : 'red';
+};
+
+const addGroceryItem = (name, price) => {
+	const groceryItem = document.createElement('div');
+	const itemNameSpan = document.createElement('span');
+	const itemPriceSpan = document.createElement('span');
+
+	groceryItem.className = 'grocery-item';
+	itemNameSpan.className = 'item-name';
+	itemPriceSpan.className = 'item-price';
+
+	itemNameSpan.innerHTML = name;
+	itemPriceSpan.innerHTML = `$${price}`;
+
+	groceryItem.appendChild(itemNameSpan);
+	groceryItem.appendChild(itemPriceSpan);
+
+	groceryList.appendChild(groceryItem);
+
+	groceryListData.push({ name, price });
+
+	updateBudget();
+};
+
+budgetForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	totalBudget = Number(budgetInput.value);
+	budgetInput.value = '';
+	budgetRemaining.innerText = totalBudget.toFixed(2);
+
+	budgetDisplay.style.backgroundColor = 'green';
+
+	itemNameInput.focus();
+});
+
+groceryForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const itemName = itemNameInput.value;
+	const itemPrice = Number(itemPriceInput.value);
+
+	console.log({ itemName, itemPrice });
+	addGroceryItem(itemName, itemPrice);
+});
